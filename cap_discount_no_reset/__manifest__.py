a69c8a427083f9ff939a4fae1a7d216b5cb27a80
captivea-ssa <56271206+captivea-ssa@users.noreply.github.com>
2019-10-18 13:22:32 -0400

    Add files via upload

---
+++ cap_discount_no_reset/__manifest__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Part of CAPTIVEA. Odoo 12 EE.
+
+{
+    'name': "cap_discount_no_reset",
+    'version': "12.0",
+    'author': "Saketha",
+    'summary': "Not to reset discount when other fields in the order line are updated",
+    'depends': [],
+    'data': [],
+    'installable': True
+}
---
+++ cap_discount_no_reset/models/name.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# Part of CAPTIVEA. Odoo 12 EE.
+
+from odoo import fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+    @api.depends('state', 'price_reduce', 'product_id', 'untaxed_amount_invoiced', 'qty_delivered')
+    def _onchange_discount(self):
+        self.discount = discount
+        
+                
