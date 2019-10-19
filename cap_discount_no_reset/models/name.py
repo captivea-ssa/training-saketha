38121462cc82cf765f3afcdcd0955f5e59e8f651
captivea-ssa <56271206+captivea-ssa@users.noreply.github.com>
2019-10-19 15:28:11 -0400

    Update name.py

--- cap_discount_no_reset/models/name.py
+++ cap_discount_no_reset/models/name.py
@@ -6,8 +6,13 @@ from odoo import fields, models
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-    @api.depends('state', 'price_reduce', 'product_id', 'untaxed_amount_invoiced', 'qty_delivered','discount')
+    #@api.depends('state', 'price_reduce', 'product_id', 'untaxed_amount_invoiced', 'qty_delivered','discount')
+    #def _onchange_discount(self):
+     #   self.discount = discount
+    @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
     def _onchange_discount(self):
-        self.discount = discount
+        if self.product_id and self.product_uom_qty>0
+            self.discount = 25
+        
         
                 
