405c1a1e97c90bfb601e185e925eeda295484aad
captivea-ssa <56271206+captivea-ssa@users.noreply.github.com>
2019-10-19 16:12:20 -0400

    Update name.py

--- cap_discount_no_reset/models/name.py
+++ cap_discount_no_reset/models/name.py
@@ -1,14 +1,14 @@
 # coding: utf-8
 # Part of CAPTIVEA. Odoo 12 EE.
 
-from odoo import fields, models
+from odoo import fields, models,api
 
 
 class DiscountModify(SaleOrderLine):
     #@api.depends('state', 'price_reduce', 'product_id', 'untaxed_amount_invoiced', 'qty_delivered','discount')
     #def _onchange_discount(self):
      #   self.discount = discount
-   # @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
+    @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
     def _onchange_discount(self):
         if self.product_id and self.product_uom_qty>0
             self.discount = 25
