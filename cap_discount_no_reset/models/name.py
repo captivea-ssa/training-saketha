6c514c7c95817324ecd433d09dadbc11b795fe46
captivea-ssa <56271206+captivea-ssa@users.noreply.github.com>
2019-10-20 11:37:24 -0400

    Update name.py

--- cap_discount_no_reset/models/name.py
+++ cap_discount_no_reset/models/name.py
@@ -6,6 +6,7 @@ from odoo import fields, models,api
 
 class Sale_Order_Line(models.Model):
     _inherit = 'sale.order.line'
+    @api.model
     @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
     def _onchange_discount(self)
         record = super(Sale_Order_Line, self)._onchange_discount()
