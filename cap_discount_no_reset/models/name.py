d73e4364872648f8250ea5b29c50b57eaf89b28b
captivea-ssa <56271206+captivea-ssa@users.noreply.github.com>
2019-10-20 13:22:31 -0400

    Update name.py

--- cap_discount_no_reset/models/name.py
+++ cap_discount_no_reset/models/name.py
@@ -4,13 +4,17 @@
 from odoo import fields, models,api
 
 
-class Sale_Order_Line(models.Model):
+class Sale_Order_Line_custom(models.Model):
+    _name = 'sale.order.line.custom'
     _inherit = 'sale.order.line'
+    customdiscount = fields.Boolean(string = 'Custom Discount class')
+    customdiscount = false
     @api.model
     @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
     def _onchange_discount(self)
         #record = super(Sale_Order_Line, self)._onchange_discount()
         self['discount'] =55.0
+        self.customdiscount = true
         return self   
         
         
